a5cf9e747a3076f146c7a7c3ff5eb339f41710f7
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-04-14 17:50:26 -0700

    Trying to deploy

--- backend/app.js
+++ backend/app.js
@@ -32,23 +32,23 @@ app.use("/users/:username/matches/like/:user_id", usersRoutes);
 app.use("/users/:username/matches/dislike/:user_id", usersRoutes);
 
 //Handles 404 errors
-app.use(function (req, res, next) {
-  const notFoundError = new NotFoundError();
-  return next(notFoundError);
+app.use(function(req, res, next) {
+    const notFoundError = new NotFoundError();
+    return next(notFoundError);
 });
 
 //Generic error handler
-app.use(function (err, req, res, next) {
-  if (process.env.NODE_ENV !== "test") console.error(err.stack);
-  const status = err.status || 500;
-  const message = err.message;
+app.use(function(err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
 
-  return res.status(status).json({
-    error: {
-      message,
-      status,
-    },
-  });
+    return res.status(status).json({
+        error: {
+            message,
+            status,
+        },
+    });
 });
 
 module.exports = app;
