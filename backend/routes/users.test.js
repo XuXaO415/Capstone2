cac8e94408f395340d78bfbadac3e95a8aa18ca1
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-04-16 15:21:32 -0700

    Made changes to these sections

--- backend/routes/users.test.js
+++ backend/routes/users.test.js
@@ -21,65 +21,6 @@ afterAll(commonAfterAll);
 
 /************************************** POST /users */
 
-// describe("POST /users", function() {
-//     test("works for admins: create non-admin", async function() {
-//         const resp = await request(app)
-//             .post("/users")
-//             .send({
-//                 username: "u-new",
-//                 firstName: "First-new",
-//                 lastName: "Last-newL",
-//                 email: "new@email.com",
-//                 password: "password-new",
-//                 city: "SF",
-//                 state: "CA",
-//                 country: "US",
-//                 zipCode: 94110,
-//                 latitude: "000000",
-//                 longitude: "000000",
-//                 interests: "hiking",
-//                 hobbies: "swimming",
-//                 isAdmin: false,
-//             })
-//             .set("authorization", `Bearer ${adminToken}`);
-//         expect(201);
-//     });
-// });
-
-// test("works for admins: create admin", async function () {
-//   const res = await request(app)
-//     .post("/users")
-//     .send({
-//       username: "u-new",
-//       firstName: "First-new",
-//       lastName: "Last-newL",
-//       password: "password-new",
-//       email: "new@email.com",
-//       city: "SF",
-//       state: "CA",
-//       country: "US",
-//       zipCode: 94110,
-//       isAdmin: true,
-//     })
-//     .set("authorization", `Bearer ${adminToken}`);
-//   expect(201);
-//   expect(res.body).toEqual({
-//     user: {
-//       username: "u-new",
-//       firstName: "First-new",
-//       lastName: "Last-newL",
-//       password: "password-new",
-//       email: "new@email.com",
-//       city: "SF",
-//       state: "CA",
-//       country: "US",
-//       zipCode: 94110,
-//     },
-//     token: expect.any(String),
-//   });
-//   expect(201);
-// });
-
 test("unauth for non-admins", async function() {
     const resp = await request(app).post("/users").send({
         username: "u-new",
@@ -116,47 +57,6 @@ test("bad request with invalid data", async function() {
 
 
 /************************************** GET /users */
-//Reconfigure this test
-// describe("GET /users", function() {
-//     test("works for admins", async function() {
-//         const resp = await request(app)
-//             .get("/users")
-//             .set("authorization", `Bearer ${adminToken}`);
-//         expect(resp.body).toEqual({
-//             users: [{
-//                     username: "admin",
-//                     firstName: "Admin",
-//                     lastName: "Istrator",
-//                     email: "admin@admin.com",
-//                     city: "SF",
-//                     state: "CA",
-//                     country: "US",
-//                     zipCode: 94110,
-//                     latitude: "000000",
-//                     longitude: "000000",
-//                     interests: "hiking",
-//                     hobbies: "swimming",
-//                     isAdmin: true,
-//                 },
-//                 {
-//                     username: "u1",
-//                     firstName: "U1F",
-//                     lastName: "U1L",
-//                     email: "user1@email.com",
-//                     city: "SF",
-//                     state: "CA",
-//                     country: "US",
-//                     zipCode: 94110,
-//                     latitude: "000000",
-//                     longitude: "000000",
-//                     interests: "hiking",
-//                     hobbies: "swimming",
-//                     isAdmin: false,
-//                 },
-//             ],
-//         });
-//     });
-
 test("unauth for non-admin users", async function() {
     const resp = await request(app)
         .get("/users")
