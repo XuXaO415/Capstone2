6f5d617f264b848372098d4938e471e416f589bc
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-04-17 16:58:39 -0700

    Cleaned up backend app routes

--- backend/app.js
+++ backend/app.js
@@ -13,25 +13,17 @@ const morgan = require("morgan");
 
 const app = express();
 
-
-// app.use(cors({
-//     origin: "https://urguide.surge.sh"
-// }));
-
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authenticateJWT);
 
-// app.use("/auth", authRoutes);
-// app.use("/users", usersRoutes);
 
-// app.use("/", usersRoutes);
+/** routes */
 app.use("/auth", authRoutes);
 app.use("/users", usersRoutes);
 app.use("/auth/token", authRoutes);
 app.use("/auth/register", authRoutes);
-
 app.use("/users/:username", usersRoutes);
 app.use("/users/username/:user_id", usersRoutes);
 app.use("/users/:username/matches", usersRoutes);
