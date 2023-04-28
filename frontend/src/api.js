972963d9a5152e3f6e740d7deb51cc3f09362f22
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-04-27 21:12:34 -0700

    Formatted API calls

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,156 +1,116 @@
 import axios from "axios";
-import { TOKEN_STORAGE_ID } from "./App";
+import {TOKEN_STORAGE_ID} from "./App";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 class UrGuideApi {
-  // store API token here
-  static token;
+    static token;
 
-  static async request(endpoint, data = {}, method = "GET") {
-    console.debug("API Call:", endpoint, data, method);
+    static async request(endpoint, data = {}, method = "GET") {
+        console.debug("API Call:", endpoint, data, method);
 
-    const _token = localStorage.getItem(TOKEN_STORAGE_ID);
+        const token = localStorage.getItem(TOKEN_STORAGE_ID);
 
-    const url = `${BASE_URL}/${endpoint}`;
-    const headers = {
-      Authorization: `Bearer ${UrGuideApi.token}`
-    };
-    const response = await axios({
-      url,
-      data,
-      method,
-      headers
-    });
-    const params = method === "GET" ? data : {};
-    console.debug("API Response:", response.data, params);
+        const url = `${BASE_URL}/${endpoint}`;
+        const headers = {
+            Authorization: `Bearer ${
+                UrGuideApi.token
+            }`
+        };
+        const response = await axios({url, data, method, headers});
+        const params = method === "GET" ? data : {};
+        console.debug("API Response:", response.data, params);
 
-    try {
-      return (
-        await axios({
-          url,
-          method,
-          data,
-          params,
-          headers
-        })
-      ).data;
-    } catch (err) {
-      console.error("API Error:", err.response);
-      let message = err.response.data.error.message;
-      throw Array.isArray(message) ? message : [message];
+        try {
+            return(await axios({
+                url,
+                method,
+                data,
+                params,
+                headers
+            })).data;
+        } catch (err) {
+            console.error("API Error:", err.response);
+            let message = err.response.data.error.message;
+            throw Array.isArray(message) ? message : [message];
+        }
     }
-  }
 
-  // Individual API calls
+    /******************** Individual API calls *******************************/
 
-  static async getCurrentUser(username) {
-    let res = await this.request(`users/${username}`);
-    return res.user;
-  }
+    static async getCurrentUser(username) {
+        let res = await this.request(`users/${username}`);
+        return res.user;
+    }
 
-  /** auth methods */
+    /* auth method
+     * - logs in user
+     */
+    static async login(data) {
+        let res = await this.request(`auth/token`, data, "POST");
+        console.debug(res, "res from login was successful");
+        return res.token;
+    }
 
-  static async login(data) {
-    let res = await this.request(`auth/token`, data, "POST");
-    console.debug(res, "res from login was successful");
-    return res.token;
-  }
+    /* auth method
+     * - registers new user 
+     */
+    static async register(data) {
+        let res = await this.request(`auth/register`, data, "POST");
+        console.debug("res from register was successful", Boolean(res.token));
+        // console.log ("Backend response from register:", res, "res from register was successful")
+        return res.token;
+    }
 
-  static async register(data) {
-    let res = await this.request(`auth/register`, data, "POST");
-    console.debug("res from register was successful", Boolean(res.token));
-    // console.log ("Backend response from register:", res, "res from register was successful")
-    return res.token;
-  }
+    /** Create a new user  */
+    static async createUser(data) {
+        let res = await this.request(`users`, data, "POST");
+        return res.user;
+    }
 
-  /** Create a new user  */
+    /* Update user profile */
+    static async updateProfile(username, data) {
+        let res = await this.request(`users/${username}`, data, "PATCH");
+        console.log(res, "res from updateProfile was successful");
+        return res.user;
+    }
 
-  static async createUser(data) {
-    let res = await this.request(`users`, data, "POST");
-    return res.user;
-  }
+    /* Get user profile */
+    static async getMatchInfo(currentUser, user_id) {
+        let res = await this.request(`users/${currentUser}/matches/user/${user_id}`, {}, "GET");
+        return res;
+    }
 
-  /* Update user profile */
+    /* Get potential matches */
+    static async getPotentialMatches(currentUser) {
+        let res = await this.request(`users/${currentUser}/matches/users`, {});
+        console.log("res from getPotentialMatches:", res, Boolean(res.users) ? "working properly...🥳" : "fix this...😭");
+        return res.users;
+    }
+    /* Get liked matches */
+    static async getLikedMatches(currentUser) {
+        let res = await this.request(`users/${currentUser}/matches/likes`, {}, "GET");
+        console.log("res from getLikedMatches:", res, "currentUser:", currentUser, Boolean(res.users) ? "🥳" : "doh...😭");
+        return res.users;
+    }
 
-  static async updateProfile(username, data) {
-    let res = await this.request(`users/${username}`, data, "PATCH");
-    console.log(res, "res from updateProfile was successful");
-    return res.user;
-  }
+    /* Post liked match */
+    static async likeMatch(currentUser, user_id) {
+        let res = await this.request(`users/${currentUser}/matches/like/${user_id}`, {}, "POST");
+        console.log("POST was successful and", {
+            currentUser
+        }, "liked", {user_id});
+        return res.status;
+    }
 
-  static async getMatchInfo(currentUser, user_id) {
-    let res = await this.request(
-      `users/${currentUser}/matches/user/${user_id}`,
-      {},
-      "GET"
-    );
-    return res;
-  }
-
-  static async getPotentialMatches(currentUser) {
-    let res = await this.request(`users/${currentUser}/matches/users`, {});
-    console.log(
-      "res from getPotentialMatches:",
-      res,
-      Boolean(res.users) ? "working properly...🥳" : "fix this...😭"
-    );
-    return res.users;
-  }
-  static async getLikedMatches(currentUser) {
-    let res = await this.request(
-      `users/${currentUser}/matches/likes`,
-      {},
-      "GET"
-    );
-    console.log(
-      "res from getLikedMatches:",
-      res,
-      "currentUser:",
-      currentUser,
-      Boolean(res.users) ? "🥳" : "doh...😭"
-    );
-    return res.users;
-  }
-
-  static async likeMatch(currentUser, user_id) {
-    let res = await this.request(
-      `users/${currentUser}/matches/like/${user_id}`,
-      {},
-      "POST"
-    );
-    console.log(
-      "POST was successful and",
-      {
-        currentUser
-      },
-      "liked",
-      {
-        user_id
-      }
-    );
-    return res.status;
-  }
-
-  static async dislikeMatch(currentUser, user_id) {
-    let res = await this.request(
-      `users/${currentUser}/matches/dislike/${user_id}`,
-      {},
-      "POST"
-    );
-    console.log(
-      "res from dislikeMatch:",
-      res,
-      {
-        currentUser
-      },
-      "disliked",
-      {
-        user_id
-      }
-    );
-    return res.status;
-  }
+    /* Post disliked match */
+    static async dislikeMatch(currentUser, user_id) {
+        let res = await this.request(`users/${currentUser}/matches/dislike/${user_id}`, {}, "POST");
+        console.log("res from dislikeMatch:", res, {
+            currentUser
+        }, "disliked", {user_id});
+        return res.status;
+    }
 }
 
 /* ************************************************************* */
