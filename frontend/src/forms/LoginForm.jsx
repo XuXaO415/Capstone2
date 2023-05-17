9388fae284445738953b1ce07a5649f234336c5c
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2023-05-16 20:00:50 -0700

    working on adding accessabilty features

--- frontend/src/forms/LoginForm.jsx
+++ frontend/src/forms/LoginForm.jsx
@@ -3,7 +3,9 @@ import { useHistory, NavLink, Redirect } from "react-router-dom";
 import UserContext from "../context/UserContext";
 import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
-import Alert from "../common/Alert";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { Container } from "react-bootstrap";
 import "./LoginForm.css";
 
 /** Form for logging in. */
@@ -47,49 +49,90 @@ function LoginForm({ login }) {
   }
 
   return (
-    <div className="LoginForm">
-      <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
-        <h2 className="mb-3">Log In</h2>
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label>Username: </label>
-            <input
-              name="username"
-              placeholder="Username"
-              className="form-control"
-              value={formData.username}
-              onChange={handleChange}
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please enter a username.
-            </Form.Control.Feedback>
-          </div>
-          <div className="form-group">
-            <label>Password: </label>
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              className="form-control"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please enter a password.
-            </Form.Control.Feedback>
-          </div>
+    // <div className="LoginForm">
+    //   <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+    //     <h1 className="mb-3">Log In</h1>
+    //     <Form validated={validated} onSubmit={handleSubmit}>
+    //       <div className="form-group">
+    //         <form label>Username: </form>
+    //         <form
+    //           type="text"
+    //           name="username"
+    //           placeholder="Username"
+    //           className="form-control"
+    //           value={formData.username}
+    //           onChange={handleChange}
+    //           required
+    //         />
+    //         <Form.Control.Feedback type="invalid">
+    //           Please enter a username.
+    //         </Form.Control.Feedback>
+    //       </div>
+    //       <div className="form-group">
+    //         <form label>Password: </form>
+    //         <form input
+    //           type="password"
+    //           placeholder="Password"
+    //           name="password"
+    //           className="form-control"
+    //           value={formData.password}
+    //           onChange={handleChange}
+    //           required
+    //         />
+    //         <Form.Control.Feedback type="invalid">
+    //           Please enter a password.
+    //         </Form.Control.Feedback>
+    //       </div>
 
-          <Button className="btn" variant="primary" type="submit">
-            Submit
+
+    //       <Button className="btn" variant="primary" type="submit">
+    //         Submit
+    //       </Button>
+    //     </Form>
+    //     <p className="mt-5 font-weight-bold">New to UrGuide? <NavLink to="/register">Sign Up</NavLink>
+    //   </p>
+    //   </div>
+    // </div>
+
+
+
+<Container className="LoginForm">
+  <div className="container mb-3">
+        <h1 className="mb-3">Sign Up</h1>
+        <Form onSubmit={handleSubmit}>
+       
+            <Form.Group className="mb-3" controlId="formGridUsername">
+              <Form.Label >Username:</Form.Label>
+              <Form.Control
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formGridPassword">
+              <Form.Label>Password:</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+   
+              <Button variant="primary" type="submit" onSubmit={handleSubmit}>
+            Sign in
           </Button>
+          <Form.Group className="mb-3" id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
         </Form>
-        <p className="mt-3">
-          New to UrGuide? <NavLink to="/register">Sign Up</NavLink>
-        </p>
+         <p className="mt-5 font-weight-bold">New to UrGuide? <NavLink to="/register">Sign Up</NavLink>
+      </p>
       </div>
-    </div>
+    </Container>
   );
 }
 
